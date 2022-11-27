76cd210a7da5124169ea331d5e92647c7422d879
Bauka <e444er@gmail.com>
2022-11-27 17:41:12 +0600

    Initial commit

---
+++ app/src/main/java/com/e444er/api/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.e444er.api
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var getUsernameButton: Button
+    private lateinit var usernameTextView: TextView
+    private lateinit var getImageButton: Button
+    private lateinit var imageFromGalleryImageView: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initViews()
+
+        val contractUsername = ActivityResultContracts.StartActivityForResult()
+        val launcherUsername = registerForActivityResult(contractUsername) {
+            if (it.resultCode == RESULT_OK) {
+                usernameTextView.text = it.data?.getStringExtra(UsernameActivity.EXTRA_USERNAME)
+            }
+        }
+
+        val contractImage = ActivityResultContracts.GetContent()
+        val launcherImage = registerForActivityResult(contractImage) {
+            imageFromGalleryImageView.setImageURI(it)
+        }
+
+        getUsernameButton.setOnClickListener {
+            launcherUsername.launch(UsernameActivity.newIntent(this))
+        }
+        getImageButton.setOnClickListener {
+            launcherImage.launch("image/*")
+        }
+    }
+
+    private fun initViews() {
+        getUsernameButton = findViewById(R.id.get_username_button)
+        usernameTextView = findViewById(R.id.username_textview)
+        getImageButton = findViewById(R.id.get_image_button)
+        imageFromGalleryImageView = findViewById(R.id.image_from_gallery_imageview)
+    }
+}
+
